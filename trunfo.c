d26ba4b996f4a9c2876a92dd9922564eca948f23
jessicalima424jl-pixel <jessicalima424.jl@gmail.com>
2025-09-01 22:32:44 -0300

    Update trunfo.c

--- trunfo.c
+++ trunfo.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+int main (){
+    char cidade [50]
+    int população
+    float Area
+    float Pib
+    int Pontos turisticos  
+printf ("whashington");
+scanf ("%s",&cidade);
 
+printf ("375,000");
+scanf ("%f",&pib);
+
+printf ("177 km");
+scanf ("%f",&area);
+
+printf ("678.972");
+scanf ("%d",&populaçao);
+
+ printf ("6");
+ scanf ("%d",&pontos turisticos);
+ printf ("cidade %s");
+ printf ("populaçao %d");
+ printf ("area %f");
+ printf ("pib %f");
+ printf ("pontos turisticos %d");
+ return 0;
+
+
+}
